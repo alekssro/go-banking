425d9d5e7f29b868490c1716abfb9aafb78adab5
alekssro <alekss.ro@gmail.com>
2021-03-24 21:14:11 +0000

    feat (config): Use env vars + sanity check

--- app/app.go
+++ app/app.go
@@ -1,16 +1,33 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/alekssro/banking/domain"
+	"github.com/alekssro/banking/logger"
 	"github.com/alekssro/banking/service"
 	"github.com/gorilla/mux"
 )
 
+func sanityCheck() {
+	if os.Getenv("BANKING_HOST") == "" {
+		logger.Warn("BANKING_HOST not defined, using default")
+		os.Setenv("BANKING_HOST", "localhost")
+	}
+	if os.Getenv("BANKING_PORT") == "" {
+		logger.Warn("BANKING_PORT not defined, using default")
+		os.Setenv("BANKING_PORT", "3000")
+	}
+}
+
 // Start the web app
 func Start() {
+
+	// Sanity Check
+	sanityCheck()
 
 	// mux := http.NewServeMux()
 	router := mux.NewRouter()
@@ -23,6 +40,8 @@ func Start() {
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 
 	// starting server
-	log.Fatal(http.ListenAndServe("localhost:8000", router))
+	host := os.Getenv("BANKING_HOST")
+	port := os.Getenv("BANKING_PORT")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), router))
 
 }
